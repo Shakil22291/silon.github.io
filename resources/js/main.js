ad7d81eadb82a52e0abaabe0b6e0e69e5534c698
Shakil22291 <64760910+Shakil22291@users.noreply.github.com>
2020-05-08 15:08:28 +0600

    Silon e-comerce template

---
+++ resources/js/main.js
@@ -0,0 +1,118 @@
+const navSlides = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".main-nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlides();
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+/* function animated() {
+  const icon = document.querySelector(".card");
+
+  icon.classList.add("animate");
+} */
+
+//product dection
+function animated1() {
+  document.getElementById("cool1").classList.add("animate");
+}
+function removeFunction1() {
+  document.getElementById("cool1").classList.remove("animate");
+}
+
+function animated2() {
+  document.getElementById("cool2").classList.add("animate");
+}
+function removeFunction2() {
+  document.getElementById("cool2").classList.remove("animate");
+}
+
+function animated3() {
+  document.getElementById("cool3").classList.add("animate");
+}
+function removeFunction3() {
+  document.getElementById("cool3").classList.remove("animate");
+}
+
+//popular dection
+function animated4() {
+  document.getElementById("cool4").classList.add("animate");
+}
+function removeFunction4() {
+  document.getElementById("cool4").classList.remove("animate");
+}
+function animated5() {
+  document.getElementById("cool5").classList.add("animate");
+}
+function removeFunction5() {
+  document.getElementById("cool5").classList.remove("animate");
+}
+function animated6() {
+  document.getElementById("cool6").classList.add("animate");
+}
+function removeFunction6() {
+  document.getElementById("cool6").classList.remove("animate");
+}
+function animated7() {
+  document.getElementById("cool7").classList.add("animate");
+}
+function removeFunction7() {
+  document.getElementById("cool7").classList.remove("animate");
+}
+
+//trending section
+function animated8() {
+  document.getElementById("cool8").classList.add("animate");
+}
+function removeFunction8() {
+  document.getElementById("cool8").classList.remove("animate");
+}
+function animated9() {
+  document.getElementById("cool9").classList.add("animate");
+}
+function removeFunction9() {
+  document.getElementById("cool9").classList.remove("animate");
+}
+function animated10() {
+  document.getElementById("cool10").classList.add("animate");
+}
+function removeFunction10() {
+  document.getElementById("cool10").classList.remove("animate");
+}
+function animated11() {
+  document.getElementById("cool11").classList.add("animate");
+}
+function removeFunction11() {
+  document.getElementById("cool11").classList.remove("animate");
+}
+function animated12() {
+  document.getElementById("cool12").classList.add("animate");
+}
+function removeFunction12() {
+  document.getElementById("cool12").classList.remove("animate");
+}
+function animated13() {
+  document.getElementById("cool13").classList.add("animate");
+}
+function removeFunction13() {
+  document.getElementById("cool13").classList.remove("animate");
+}
+function animated14() {
+  document.getElementById("cool14").classList.add("animate");
+}
+function removeFunction14() {
+  document.getElementById("cool14").classList.remove("animate");
+}
+function animated15() {
+  document.getElementById("cool15").classList.add("animate");
+}
+function removeFunction15() {
+  document.getElementById("cool15").classList.remove("animate");
+}
